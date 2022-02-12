afd6ab5a6369ef8bb9ac9bef06c6d8ef8a81058c
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-02-12 12:57:40 +0100

    Handled `AddTodoInput` clear

--- src/App/Components/AddTodoInput.purs
+++ src/App/Components/AddTodoInput.purs
@@ -8,6 +8,8 @@ import Effect.Uncurried (EffectFn1)
 import React.Basic.DOM as DOM
 import React.Basic.DOM.Events (key, targetValue)
 import React.Basic.Events (SyntheticEvent, handler, merge)
+import React.Basic.Hooks (useState, (/\))
+import React.Basic.Hooks as React
 
 type Props
   = { onAdd :: String -> Effect Unit }
@@ -15,11 +17,27 @@ type Props
 mkAddTodoInput :: AppComponent Props
 mkAddTodoInput =
   appComponent "AddTodoInput" \{ onAdd } -> React.do
+    value /\ setValue <- useState ""
     let
       handleAdd :: EffectFn1 SyntheticEvent Unit
       handleAdd =
         handler (merge { key, targetValue }) \event -> case event of
-          { key: Just "Enter", targetValue: Just v } -> onAdd v
+          { key: Just "Enter", targetValue: Just v } -> do
+            _ <- onAdd v
+            _ <- setValue \_ -> ""
+            pure unit
+          _ -> pure unit
+
+      handleChange :: EffectFn1 SyntheticEvent Unit
+      handleChange =
+        handler targetValue \maybeValue -> case maybeValue of
+          Just v -> setValue \_ -> v
           _ -> pure unit
     pure
-      $ DOM.input { type: "text", autoFocus: true, onKeyUp: handleAdd }
+      $ DOM.input
+          { type: "text"
+          , autoFocus: true
+          , value
+          , onChange: handleChange
+          , onKeyUp: handleAdd
+          }
