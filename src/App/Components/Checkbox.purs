66c503a8129b537c59cac242aa172100bd3370ed
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-02-18 19:47:15 +0100

    Separated `Checkbox

---
+++ src/App/Components/Checkbox.purs
@@ -0,0 +1,23 @@
+module App.Components.Checkbox where
+
+import Prelude
+import AppComponent (AppComponent, appComponent)
+import Effect (Effect)
+import Effect.Uncurried (mkEffectFn1)
+import React.Basic.DOM as DOM
+
+type Props
+  = { checked :: Boolean
+    , onChange :: Boolean -> Effect Unit
+    }
+
+mkCheckbox :: AppComponent Props
+mkCheckbox = do
+  appComponent "Checkbox" \{ checked, onChange } -> React.do
+    pure
+      $ DOM.input
+          { className: "appearance-none checked:bg-emerald-500 bg-slate-200 w-4 h-4 rounded"
+          , type: "checkbox"
+          , checked
+          , onChange: mkEffectFn1 \_ -> onChange $ not checked
+          }
--- src/App/Components/Todo.purs
+++ src/App/Components/Todo.purs
@@ -1,10 +1,10 @@
 module App.Components.Todo where
 
 import Prelude
+import App.Components.Checkbox (mkCheckbox)
 import App.Components.Link (mkLink)
 import AppComponent (AppComponent, appComponent)
 import Effect (Effect)
-import Effect.Uncurried (mkEffectFn1)
 import React.Basic.DOM as DOM
 import State.Todo (Todo)
 
@@ -14,18 +14,16 @@ type Props
 mkTodo :: AppComponent Props
 mkTodo = do
   link <- mkLink
+  checkbox <- mkCheckbox
   appComponent "Todo" \{ todo, onChangeStatus } -> React.do
     pure
       $ DOM.div
-          { className: "flex flex-row gap-x-3 align-center"
+          { className: "bg-white py-2 px-4 rounded flex flex-row gap-x-3 items-center"
           , children:
-              [ DOM.div_
-                  [ DOM.input
-                      { type: "checkbox"
-                      , checked: todo.checked
-                      , onChange: mkEffectFn1 \_ -> onChangeStatus $ not todo.checked
-                      }
-                  ]
+              [ checkbox
+                  { checked: todo.checked
+                  , onChange: onChangeStatus
+                  }
               , DOM.div_
                   [ link
                       { route: "/" <> todo.id
