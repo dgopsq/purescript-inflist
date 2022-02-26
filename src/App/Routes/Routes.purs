56a4b10d53dd3596cb57e832244a52a0c286b206
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-02-26 15:19:58 +0100

    Added `Routes` doc comments

--- src/App/Routes/Routes.purs
+++ src/App/Routes/Routes.purs
@@ -1,20 +1,22 @@
 module App.Routes where
 
 import Prelude
+import App.State.Todo (TodoId)
 import Data.Foldable as Foldable
 import Data.Maybe (Maybe(..))
 import React.Basic (ReactContext)
 import Routing.Match (Match)
 import Routing.Match as Match
 import Routing.PushState (PushStateInterface)
-import App.State.Todo (TodoId)
 
--- | The App routes structure.
+-- | This is the data structure that describes the
+-- | application's routes.
 data AppRoute
   = RootTodos
   | ChildrenTodos TodoId
 
--- | The App routes parser.
+-- | The parser used to match a string
+-- | with a route from the `AppRoute` sum type.
 appRoute :: Match (Maybe AppRoute)
 appRoute =
   Foldable.oneOf
@@ -30,15 +32,21 @@ appRoute =
           ]
       <* Match.end
 
--- | We have a `Maybe` here because the context has a default
--- | value and this would be a problem for the routing.
+-- | The type describing a React context that contains
+-- | the application's routes utilities.
 type RouterContext
   = ReactContext (Maybe RouterContextValue)
 
+-- | The data structure containing all the routing utilities.
+-- | This Record contains the current route and the 
+-- | navigation interface used to manage the browser native
+-- | `PushState` navigation.
 type RouterContextValue
   = { route :: Maybe AppRoute
     , nav :: PushStateInterface
     }
 
+-- | The type used in the `Reader` monad to
+-- | describe all the dependencies related to routing.
 type Router
   = { routerContext :: RouterContext }
